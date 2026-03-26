5c159e8e4c0e18d312ac89a187247fcce666cab0
sebbass28 <juaagucas@alu.edu.gva.es>
2026-03-26 20:07:40 +0100

    Final Fix: resolve UserViewSet crash and sync clean Dentalink build

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,4 @@
-from django.urls import path, include
-from rest_framework.routers import SimpleRouter
-from . import views
-
-router = SimpleRouter()
-router.register(r'', views.UserViewSet, basename='user')
-
 urlpatterns = [
-    path('signup-web/', views.signup, name='signup'),
-    path('', include(router.urls)),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
